5ce2130be3bac34dcb5ba6b4f47ad2ef41c010fd
perfogic <longdieu12x@gmail.com>
2025-11-10 14:20:27 +0700

    feat: add readme.md

--- apps/server/src/solana/index.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SolanaController } from './index.controller';
-import { SolanaService } from './index.service';
-
-@Module({
-  controllers: [SolanaController],
-  providers: [SolanaService],
-})
-export class SolanaModule {}
--- apps/server/src/solana/index.types.ts
+++
@@ -1,4 +0,0 @@
-export interface TxCountResult {
-  blockNumber: number;
-  transactionCount: number;
-}
